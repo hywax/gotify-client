e9fec726fd11c4dabdf74fe96ce3e089acaef6ad
hywax <me@hywax.space>
2024-01-26 19:08:44 +0500

    chore(builder): types generator

--- src/builder/generators/types.ts
+++ src/builder/generators/types.ts
@@ -1,5 +1,19 @@
 import type { GenerateApiOutput } from 'swagger-typescript-api'
+import { getDirnameNormalize } from '../utils/files'
 
-export default function (_api: GenerateApiOutput): void {
+export default async function (api: GenerateApiOutput): Promise<void> {
+  const typesContent = api.files.reduce((acc, file) => {
+    acc += file.fileContent
 
+    return acc
+  }, '')
+
+  const customTemplate = api.getTemplate({ path: getDirnameNormalize(import.meta.url, '../templates/types.ejs') })
+  const customContent = api.renderTemplate(customTemplate, {})
+
+  api.createFile({
+    fileName: `types.ts`,
+    path: getDirnameNormalize(import.meta.url, '../..'),
+    content: await api.formatTSContent(typesContent + customContent),
+  })
 }
---
+++ src/builder/templates/types.ejs
@@ -0,0 +1,21 @@
+export type QueryParamsType = Record<string | number, any>
+export type HttpFormat = keyof Omit<Body, 'body' | 'bodyUsed'>
+
+export enum HttpContentType {
+  Json = 'application/json',
+  FormData = 'multipart/form-data',
+  UrlEncoded = 'application/x-www-form-urlencoded',
+  Text = 'text/plain',
+}
+
+export interface HttpClient {
+  request<Result>(url: string, params?: HttpRequestParams): Promise<Result>
+}
+
+export interface HttpRequestParams {
+  query?: QueryParamsType
+  body?: unknown
+  method?: string
+  type?: HttpContentType
+  format?: HttpFormat
+}
