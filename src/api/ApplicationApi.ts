38ae5fbada20612eff72ecc254fec3a6a2e3baab
hywax <me@hywax.space>
2024-01-26 20:44:28 +0500

    feat: application api

---
+++ src/api/ApplicationApi.ts
@@ -0,0 +1,105 @@
+import type {
+  Application,
+  ApplicationParams,
+  GetAppMessagesParams,
+  HttpClient,
+  PagedMessages,
+  UploadAppImagePayload } from '../types'
+import {
+  HttpContentType,
+} from '../types'
+
+export class ApplicationApi {
+  private httpClient: HttpClient
+
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient
+  }
+
+  /**
+   * Return all applications.
+   */
+  public getApps(): Promise<Application[]> {
+    return this.httpClient.request<Application[]>(`/application`, {
+      method: 'GET',
+      type: HttpContentType.Json,
+      format: 'json',
+    })
+  }
+
+  /**
+   * Create an application.
+   */
+  public createApp(body: ApplicationParams): Promise<Application> {
+    return this.httpClient.request<Application>(`/application`, {
+      method: 'POST',
+      body,
+      type: HttpContentType.Json,
+      format: 'json',
+    })
+  }
+
+  /**
+   * Update an application.
+   */
+  public updateApplication(id: number, body: ApplicationParams): Promise<Application> {
+    return this.httpClient.request<Application>(`/application/${id}`, {
+      method: 'PUT',
+      body,
+      type: HttpContentType.Json,
+      format: 'json',
+    })
+  }
+
+  /**
+   * Delete an application.
+   */
+  public deleteApp(id: number): Promise<void> {
+    return this.httpClient.request<void>(`/application/${id}`, {
+      method: 'DELETE',
+      type: HttpContentType.Json,
+    })
+  }
+
+  /**
+   * Upload an image for an application.
+   */
+  public uploadAppImage(id: number, data: UploadAppImagePayload): Promise<Application> {
+    return this.httpClient.request<Application>(`/application/${id}/image`, {
+      method: 'POST',
+      body: data,
+      type: HttpContentType.FormData,
+      format: 'json',
+    })
+  }
+
+  /**
+   * Deletes an image of an application.
+   */
+  public removeAppImage(id: number): Promise<void> {
+    return this.httpClient.request<void>(`/application/${id}/image`, {
+      method: 'DELETE',
+      type: HttpContentType.Json,
+    })
+  }
+
+  /**
+   * Return all messages from a specific application.
+   */
+  public getAppMessages({ id, ...query }: GetAppMessagesParams): Promise<PagedMessages> {
+    return this.httpClient.request<PagedMessages>(`/application/${id}/message`, {
+      method: 'GET',
+      query,
+      format: 'json',
+    })
+  }
+
+  /**
+   * Delete all messages from a specific application.
+   */
+  public deleteAppMessages(id: number): Promise<void> {
+    return this.httpClient.request<void>(`/application/${id}/message`, {
+      method: 'DELETE',
+    })
+  }
+}
