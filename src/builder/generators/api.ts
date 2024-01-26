73e918f8803259a308f95025f6c18ca7a241c93d
hywax <me@hywax.space>
2024-01-26 19:31:20 +0500

    chore(builder): types api

--- src/builder/generators/api.ts
+++ src/builder/generators/api.ts
@@ -1,5 +1,21 @@
 import type { GenerateApiOutput } from 'swagger-typescript-api'
+import capitalize from 'lodash/capitalize'
+import { getDirnameNormalize } from '../utils/files'
 
-export default function (_api: GenerateApiOutput): void {
+export default async function (api: GenerateApiOutput): Promise<void> {
+  for (const route of api.configuration.routes.combined!) {
+    const template = api.getTemplate({ path: getDirnameNormalize(import.meta.url, '../templates/api.ejs') })
+    const content = api.renderTemplate(template, {
+      name: route.moduleName,
+      routes: route.routes,
+      modelTypes: api.configuration.modelTypes,
+      apiMethodPathEjs: getDirnameNormalize(import.meta.url, '../templates/api-method.ejs'),
+    })
 
+    api.createFile({
+      fileName: `${capitalize(route.moduleName)}Api.ts`,
+      path: getDirnameNormalize(import.meta.url, '../../api'),
+      content: await api.formatTSContent(content),
+    })
+  }
 }
