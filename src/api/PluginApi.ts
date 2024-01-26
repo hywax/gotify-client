71dc4f377dff18b4c5f0eb4c402c739ab4722e84
hywax <me@hywax.space>
2024-01-26 20:45:11 +0500

    feat: plugin api

---
+++ src/api/PluginApi.ts
@@ -0,0 +1,71 @@
+import type { HttpClient, PluginConf } from '../types'
+import { HttpContentType } from '../types'
+
+export class PluginApi {
+  private httpClient: HttpClient
+
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient
+  }
+
+  /**
+   * Return all plugins.
+   */
+  public getPlugins(): Promise<PluginConf[]> {
+    return this.httpClient.request<PluginConf[]>(`/plugin`, {
+      method: 'GET',
+      type: HttpContentType.Json,
+      format: 'json',
+    })
+  }
+
+  /**
+   * Get YAML configuration for Configurer plugin.
+   */
+  public getPluginConfig(id: number): Promise<object> {
+    return this.httpClient.request<object>(`/plugin/${id}/config`, {
+      method: 'GET',
+      type: HttpContentType.Json,
+    })
+  }
+
+  /**
+   * Update YAML configuration for Configurer plugin.
+   */
+  public updatePluginConfig(id: number): Promise<void> {
+    return this.httpClient.request<void>(`/plugin/${id}/config`, {
+      method: 'POST',
+    })
+  }
+
+  /**
+   * Disable a plugin.
+   */
+  public disablePlugin(id: number): Promise<void> {
+    return this.httpClient.request<void>(`/plugin/${id}/disable`, {
+      method: 'POST',
+      type: HttpContentType.Json,
+    })
+  }
+
+  /**
+   * Get display info for a Displayer plugin.
+   */
+  public getPluginDisplay(id: number): Promise<string> {
+    return this.httpClient.request<string>(`/plugin/${id}/display`, {
+      method: 'GET',
+      type: HttpContentType.Json,
+      format: 'json',
+    })
+  }
+
+  /**
+   * Enable a plugin.
+   */
+  public enablePlugin(id: number): Promise<void> {
+    return this.httpClient.request<void>(`/plugin/${id}/enable`, {
+      method: 'POST',
+      type: HttpContentType.Json,
+    })
+  }
+}
